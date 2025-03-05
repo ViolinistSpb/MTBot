1b2942fec9f61902d637c3d6ae7d7dcaa6b8b236
Vitalii Malkov <malkoff@inbox.ru>
2025-03-05 14:55:48 +0300

    before new parsing

--- asus_bot/parsing_new.py
+++ asus_bot/parsing_new.py
@@ -1,3 +1,4 @@
+from urllib.parse import urljoin
 from dotenv import load_dotenv
 import os
 
@@ -30,8 +31,8 @@ MAIN_URL = 'https://rep.mariinsky.ru/'
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
-def recieve_schedule(username, password, days):
-    response = get_response(username, password)
+def recieve_schedule(username, password, days):   # add session
+    response, session = get_response(username, password)   # add session
     soup = BeautifulSoup(response.text, 'lxml')
     # surname finding block
     li = soup.find('li', attrs={'id': 'menuLogin'})
@@ -41,8 +42,14 @@ def recieve_schedule(username, password, days):
     table = soup.find('table', attrs={'class': 'table table-hover'})
     all_tr = table.find_all('tr')
     message = ''
+    links_list = []  # инициализация списка ссылок
     from_second_tr = all_tr[1:8]
     for tr in from_second_tr:
+        td = tr.find_all('td')[1]
+        links = td.find_all('a')
+        if links:  # если есть ссылки, добавляем их в список
+            for link in links:
+                links_list.append(urljoin(MAIN_URL, link['href']))
         div = tr.find('div').text
         message += '%'
         message += div
@@ -51,10 +58,27 @@ def recieve_schedule(username, password, days):
             message += " ".join(
                 line.strip() for line in d.text.splitlines() if line.strip())
     list_message = message.split('%')
+    list_message = list_message[:1+int(days)]
     final_message = ''
-    for day in list_message[:1+int(days)]:
-        final_message += day
-    return clean_text(final_message), surname
+    # for day in list_message:
+    #     final_message += day
+    places = find_place(session, surname, links_list)  # отправляем в функцию парсинга мест
+    print(f'len(places): {len(places)}')
+    print(f'len(list_message): {len(list_message)}')
+    print(places)
+    for i in range(len(list_message)):
+        print(list_message[i], i)
+        final_message += list_message[i]
+        try:
+            print(places[i])
+            if 'Выходной' not in list_message[i].split():
+                if isinstance(places[i], tuple):
+                    final_message += ", ".join(places[i])
+                if isinstance(places[i], str):
+                    final_message += places[i]
+        except IndexError:
+            None
+    return clean_text(final_message)
 
 
 def get_response(username, password):
@@ -79,6 +103,27 @@ def get_response(username, password):
     if soup.find('hgroup', attrs={'class': 'title'}):
         return False
     if soup.find('table', attrs={'class': 'table table-hover'}):
-        return response
+        return response, session  # add session
     else:
-        return response
+        return response, session  # add session
+
+
+def find_place(session, surname, links_list):
+    places = []
+    for link in links_list:
+        response = session.get(link)
+        soup = BeautifulSoup(response.text, 'lxml')
+        divs = soup.find_all('div', class_='color0')
+        maestro = divs[0].text.strip()
+        if len(maestro.split()) > 1:
+            maestro = ''
+        for div in divs:
+            if surname in div.text:
+                place = div.find('span').text + ' место'
+                if maestro != '':
+                    maestro = 'Дир. ' + maestro
+                    places.append((maestro, place))
+                else:
+                    places.append(place)
+                break
+    return places  # Возвращает список кортежей (дирижер, место пользователя)
